b81d0d6984e287c430f020e2892b5b49128c8a64
JuanviGirbes <94296484+JuanviGirbes@users.noreply.github.com>
2022-01-16 20:55:16 +0100

    JuanviGirbes
    modificacion desde githubweb

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -15,6 +15,9 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    } 
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
     
 }
